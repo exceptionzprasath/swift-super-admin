95ba96434991d92869652c968bbf294ffb36d755
exceptionzprasath <exceptionzprasath@gmail.com>
2026-07-20 18:33:33 +0530

    added vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+if (process.env.VERCEL) {
+  process.env.NITRO_PRESET = "vercel";
+}
+
 export default defineConfig({
   plugins: [
     tanstackStart({
